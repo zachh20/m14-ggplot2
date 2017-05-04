ece58abab46381c8c6fe4f59daefc25ccb202cdd
zachh20 <zachh20@msn.com>
2017-05-04 14:49:45 -0700

    participation

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,17 +2,18 @@
 
 # Install and load `ggplot2`
 # You will also want to load `dplyr`
-
-
+install.packages("ggplot2")
+library(ggplot2)
+library(dplyr)
 # For this exercise you'll be working with the `diamonds` data set included in the ggplot2 library
 # Use `?diamonds` to get more information about this data set (including the column descriptions
 # Also check the _column names_ and the _number of rows_ in the data set
-
+??diamonds
 
 # This data set has a lot of rows. To make things a bit more readable,
 # use dplyr's `sample_n()` function to get a random 1000 rows from the data set
 # Store this sample in a variable `diamonds.sample`
-
+diamond.sample <- sample_n(diamonds, 1000)
 
 # Start by making a new `ggplot` with the `diamonds.sample` as the data (no geometry yet)
 # What do you see?
